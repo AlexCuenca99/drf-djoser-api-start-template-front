6fc52104b2d327ef136b31c91a80a52f003ec2d5
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-23 12:48:09 -0500

    Layout of the AuthLayout

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
 
-import { useAuth } from 'hooks';
 export function Homepage() {
-	const { logout } = useAuth();
 	return (
 		<div>
 			<h1>Home</h1>
-			<Button onClick={logout}>Logout</Button>
 		</div>
 	);
 }
