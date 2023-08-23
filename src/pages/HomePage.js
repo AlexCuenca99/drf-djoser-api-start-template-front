35a8805a6e725456c92cf7b89e23e7e7268554be
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-23 10:31:30 -0500

    Add logout button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
+import { useAuth } from 'hooks';
 export function Homepage() {
+	const { logout } = useAuth();
 	return (
 		<div>
 			<h1>Home</h1>
+			<Button onClick={logout}>Logout</Button>
 		</div>
 	);
 }
